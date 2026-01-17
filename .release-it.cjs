05e0c3a24d76cd93a7ca8fe137faebde452d42a8
Anjey Tsibylskij <130153594+atldays@users.noreply.github.com>
2026-01-18 01:08:12 +0200

    ci(release): enhance npm publishing and release workflow configuration

--- .release-it.cjs
+++ .release-it.cjs
@@ -139,8 +139,11 @@ module.exports = () => {
 
         npm: {
             publish: true,
+            skipChecks: true,
+            provenance: true,
+            access: "public",
+            registry: "https://registry.npmjs.org/",
             versionArgs: ["--no-git-tag-version"],
-            publishArgs: ["--provenance", "--access", "public"],
         },
 
         plugins: {
